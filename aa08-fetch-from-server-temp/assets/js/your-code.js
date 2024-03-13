93a6659109c9010a9fc7037693f0d38968e9431e
Khaliph Green <kgreen375@gmail.com>
2024-03-12 23:10:15 -0500

    Phase 1, 2, and 3 complete

--- aa08-fetch-from-server-temp/assets/js/your-code.js
+++ aa08-fetch-from-server-temp/assets/js/your-code.js
@@ -1,19 +1,30 @@
 export function getAllDogs() {
-    // Your code here 
+  return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here 
+  return fetch("/dogs/2");
 }
 
 export function postNewDog() {
-    // Your code here 
+  const params = new URLSearchParams({
+    name: "Max",
+    age: 2,
+  });
+
+  return fetch("/dogs", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: params,
+  });
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here 
+  // Your code here
 }
 
 export function deleteDog(id) {
-      // Your code here 
+  // Your code here
 }
